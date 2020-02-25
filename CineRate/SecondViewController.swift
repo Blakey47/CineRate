6701f06efa8da2c2e2f9aab4767fbb5a2afc4779
Darragh <darragh.blake@urbanairship.com>
2020-02-25 14:07:24 +0000

    Initial Commit

---
+++ CineRate/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CineRate
+//
+//  Created by Darragh Blake on 25/02/2020.
+//  Copyright © 2020 Darragh Blake. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
